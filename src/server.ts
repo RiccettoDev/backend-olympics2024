dc33f77f787850dffa7a89679f5a505b54df7a66
Eduardo <princesherlockk@gmail.com>
2024-04-04 11:31:36 -0300

    [backend-olympics2024]-fix- Adjust port in server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const start = async () => {
   await app.register(routes)
 
   try {
-    await app.listen({ port:3333 })
+    await app.listen({ port: 3333, host: '0.0.0.0' })
     console.log('API running...');
   } catch (error) {
     process.exit(1)
